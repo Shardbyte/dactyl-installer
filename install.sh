2116a81ddd1152833d90c1a9f977a256e00a9892
Shardbyte <saint@shardbyte.com>
2025-05-02 00:39:23 -0500

    version update

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@
 
 set -e
 
-export GITHUB_SOURCE="v1.2.3"
-export SCRIPT_RELEASE="v1.2.3"
+export GITHUB_SOURCE="v1.2.5"
+export SCRIPT_RELEASE="v1.2.5"
 export GITHUB_BASE_URL="https://raw.githubusercontent.com/Shardbyte/dactyl-installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
